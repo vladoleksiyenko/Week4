5d2850910ae7b91f04b09fd0c2a528675befc774
Vlad Oleksiyenko <vladolek2003@gmail.com>
2024-04-23 13:29:48 -0400

    Added key value pairs and displayed them on the html page

--- index.php
+++ index.php
@@ -14,9 +14,27 @@
 
     // Define a default route
     // https://voleksiyenko.greenriverdev.com/328/week4/
-    $f3->route('GET /', function() {
+    $f3->route('GET /', function($f3) {
+
+        // Add data to the Fat-Free "hive"
+        $f3->set('name', 'Vlad');
+        $f3->set('lastinitial', 'O');
+        $f3->set('age', 20);
+
+        $fruits = array('apple', 'banana', 'orange');
+        $f3->set('fruitList', $fruits);
+
+        $cars = array('ford', 'bmw', 'toyota');
+        $f3->set('carList', $cars);
+
+        $students = array('101'=>'Mason', '102'=>'Tilak', '103'=>'Sage');
+        $f3->set('studentList', $students);
+
+
         // Render a view page
         $view = new Template();
         echo $view->render('views/info.html');
     });
+
+    $f3->run();
 ?>
